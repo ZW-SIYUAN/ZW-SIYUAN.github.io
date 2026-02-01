4b77e3e1f8de98f6fa40ceb3a8140fd867c0af14
ZW-SIYUAN <zwang68@wm.edu>
2026-02-01 14:02:35 +0000

    Deploying to gh-pages from @ ZW-SIYUAN/ZW-SIYUAN.github.io@08cc09fcc6aa08273fe5da31fc70dac9c8b28764 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "ZW-SIYUAN/ZW-SIYUAN.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
